b96cf69290bdaa45ed4844008245d2a78b659fc1
Iury Souza <iurysza@gmail.com>
2021-12-06 08:21:51 -0300

    code cleanup

--- src/Day02.kt
+++ src/Day02.kt
@@ -9,56 +9,47 @@ fun main() {
 }
 
 
-fun partOne(list: List<String>): Int = mapCommands(list).reduce { acc, commandLength ->
+fun partOne(input: List<String>): Int = parseCommands(input).reduce { acc, commandLength ->
     val command = commandLength.keys.first()
     val length = commandLength.values.first()
 
-    val vertical = acc.getOrDefault("vertical", 0)
-    val forward = acc.getOrDefault("forward", 0)
+    val vertical = acc.getVertical()
+    val forward = acc.getForward()
     when (command) {
-        "up" -> {
-            acc["vertical"] = vertical + length * (-1)
-        }
-        "down" -> {
-            acc["vertical"] = vertical + length
-        }
-        else -> {
-            acc["forward"] = forward + length
-        }
+        "up" -> acc["vertical"] = vertical + length * (-1)
+        "down" -> acc["vertical"] = vertical + length
+        else -> acc["forward"] = forward + length
     }
     acc
-}.let {
-    it.getOrDefault("forward", 0) * it.getOrDefault("vertical", 0)
-}
+}.let { it.getForward() * it.getVertical() }
 
 
 fun partTwo(list: List<String>): Int {
     var aim = 0
-    return mapCommands(list).reduce { acc, commandLength ->
+    return parseCommands(list).reduce { acc, commandLength ->
         val command = commandLength.keys.first()
         val length = commandLength.values.first()
 
-        val vertical = acc.getOrDefault("vertical", 0)
-        val forward = acc.getOrDefault("forward", 0)
+        val vertical = acc.getVertical()
+        val forward = acc.getForward()
         when (command) {
-            "up" -> {
-                aim -= length
-            }
-            "down" -> {
-                aim += length
-            }
+            "up" -> aim -= length
+            "down" -> aim += length
             else -> {
                 acc["vertical"] = vertical + length * aim
                 acc["forward"] = forward + length
             }
         }
         acc
-    }.let {
-        it.getOrDefault("forward", 0) * it.getOrDefault("vertical", 0)
-    }
+    }.let { it.getForward() * it.getVertical() }
 }
 
-private fun mapCommands(list: List<String>) = list.map { text ->
+
+private fun MutableMap<String, Int>.getForward(): Int = getOrDefault("forward", 0)
+
+private fun MutableMap<String, Int>.getVertical(): Int = getOrDefault("vertical", 0)
+
+private fun parseCommands(list: List<String>) = list.map { text ->
     val command = "\\w+".toRegex().find(text)?.value?.trim() ?: throw InvalidAlgorithmParameterException()
     val length = text.replace(command, "").trim().toInt()
     command to length
